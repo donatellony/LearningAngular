b1a1fe32d2c58106ece909680e7c0576c04429e4
donatellony <donatellony4736@gmail.com>
2020-12-10 17:20:44 +0200

    Two Way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Dynamic title';
-  number = 42;
-  arr = [1, 2, 3];
-  // reactImg = 'https://reactjs.org/logo-og.png';
-  inputValue = '';
-  constructor() {
-    // setTimeout(() => {
-    // tslint:disable-next-line:max-line-length
-    //   this.reactImg = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png';
-    // }, 5000);
-  }
-  onInput(event : KeyboardEvent){
-    this.inputValue = (<HTMLInputElement>event.target).value
-  }
-  onClick(){
-    console.log('CLICK!')
-  }
-  onBlur(str: string){
-    this.inputValue = str;
+  title = 'Initial';
+
+  onInput(event:any){
+    this.title=event.target.value;
   }
 }
