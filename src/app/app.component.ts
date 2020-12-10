eb973921a63eee798c083680708e0030cef07323
donatellony <donatellony4736@gmail.com>
2020-12-10 18:20:37 +0200

    Trying out Pipes. This is the end of Angular basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  arr = [1,1,2,3,5,8,13]
-
-  objs = [
-    {title: 'Post 1', author: 'donatellony', comments: [
-        {name: 'Yehor', text: 'lala 1'},
-        {name: 'Yehor', text: 'lala 2'},
-        {name: 'Yehor', text: 'lala 3'}
-      ]},
-    {title: 'Post 2', author: 'donatellony 2', comments: [
-        {name: 'Yehor 2', text: 'lala 1'},
-        {name: 'Yehor 2', text: 'lala 2'},
-        {name: 'Yehor 2', text: 'lala 3'}
-      ]}
-  ]
+  now: Date = new Date()
 }
