63e6fe682ca986a09df176e68cf06dfa9d82a2b1
donatellony <donatellony4736@gmail.com>
2021-02-06 20:12:03 +0200

    Test commit

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,11 +1,30 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
+  @Input('appStyle') color: string = 'blue';
+  @Input() fontWeight = 'normal';
+
+  @HostBinding('style.color') elColor = null;
   constructor(private elRef: ElementRef, private r: Renderer2) {
-    this.r.setStyle(elRef.nativeElement, 'color', 'blue');
     // elRef.nativeElement.style.color = 'red';
+  }
+
+  @HostListener('click', ['$event']) onClick(event: Event){
+
+  }
+
+  @HostListener('mouseenter') onEnter(){
+    // this.r.setStyle(this.elRef.nativeElement, 'color', this.color);
+    // this.r.setStyle(this.elRef.nativeElement, 'fontWeight', this.fontWeight);
+    this.elColor = this.color;
+  }
+
+  @HostListener('mouseleave') onLeave(){
+    // this.r.setStyle(this.elRef.nativeElement, 'color', null);
+    // this.r.setStyle(this.elRef.nativeElement, 'fontWeight', null);
+    this.elColor = null;
   }
 }
